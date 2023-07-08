e882beef8f0087e100d1bf24bf823973f896d934
mi nombre <tomasgp2018@gmail.comm>
2023-07-07 20:45:16 -0500

    se crea proyecto

---
+++ app/src/main/java/com/example/pruebapueblofinal/Listah.java
@@ -0,0 +1,14 @@
+package com.example.pruebapueblofinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Listah extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listah);
+    }
+}
